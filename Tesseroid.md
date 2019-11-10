e9270acd56094ecb7b4ec5b432123eddefb731e0
EC2 Default User <ec2-user@ip-172-31-44-6.eu-central-1.compute.internal>
2019-11-10 08:26:56 +0000

    added md, renamed main

---
+++ Tesseroid.md
@@ -0,0 +1,7 @@
+# POST request example
+curl -X POST https://mcbn0jkd8l.execute-api.eu-central-1.amazonaws.com/dev/ocr -d "https://i.imgur.com/zGRdhAT.jpg"
+
+# API architecture for POST 
+1. parameters: config,extension
+2. Use form-data instead of x-www-form because it supports files also.
+3. pytesseract directly accepts image files, not URLs. But tesseract command line takes url path
--- example/handler.py
+++ example/handler.py
@@ -2,12 +2,14 @@ from PIL import Image
 import requests
 import pytesseract
 from io import BytesIO
+import json
 
-
-def main(evt, ctx):
-    url = evt["body"]
+def main(event, context):
+    url = event["body"]
     res = requests.get(url)
     img = Image.open(BytesIO(res.content))
-    txt = pytesseract.image_to_string(img, lang="deu")
+    txt = pytesseract.image_to_string(img, lang="eng")
+    # data=json.loads(event["body"])
+    # txt=data["lang"]
     return {"statusCode": 200, "body": txt}
 
