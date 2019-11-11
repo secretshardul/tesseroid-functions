77c1382a2c9fc01b5f7c4805e1b33c20019b52fe
secretshardul <shardul.aeer@somaiya.edu>
2019-11-11 12:35:16 +0530

    changed import statement

--- handler.py
+++ handler.py
@@ -1,12 +1,15 @@
 import pytesseract
-import urllib
+import urllib.request
 
 def main(event, context):
     url = event["body"]
+    # url="https://media3.giphy.com/media/D0Uhua2Z1PC8w/source.gif"
+    # url="https://i.imgur.com/zGRdhAT.jpg"
     # better save to /tmp/img then use OCR
     with urllib.request.urlopen(url) as response, open("/tmp/img", 'wb') as out_file: #no .extension needed
         data = response.read() # a `bytes` object
         out_file.write(data)
+    
     txt = pytesseract.image_to_string("/tmp/img", lang="eng")
 
     # print(txt)
